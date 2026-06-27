da3bdd7c40ea5fdc1f8e0242f6f7e445ddc9d714
Alessandro Ottone <alessandro180470@gmail.com>
2026-06-27 19:28:37 +0200

    Terminata lezione dei moduli fs

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Questa sezione è indicata per lo studio e la compresnsione di node
+Questa sezione è indicata per lo studio e la compresnsione di node.
+
+**Moduli
+**Modulo fs sincrono e asincrono
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // modulo fs file system
-const{readFileSync,writeFileSync} = require('fs')
-const prova = readFileSync('./cartella/prova.txt','utf8');
-const ciao = readFileSync('./cartella/ciao.txt','utf8');
+//const{readFileSync,writeFileSync} = require('fs')
+//const prova = readFile('./cartella/prova.txt','utf8');
+/* const ciao = readFileSync('./cartella/ciao.txt','utf8');
 
 console.log(ciao)
 console.log(prova)
@@ -9,5 +9,14 @@ console.log(prova)
 writeFileSync('./cartella/prova.txt',' abito in via enrico mazzoccolo',{flag: 'a'})//inserendo uno scritto quest'ultimo va a sovrascrivere su quello vecchio
 
 
-writeFileSync('./cartella/alessandro.txt','Ciao da Alessandro');
+writeFileSync('./cartella/alessandro.txt','Ciao da Alessandro');//per creare una nuova cartella 
 
+ */
+const{readFile,writeFile} = require('fs')
+readFile('./cartella/prova.txt','utf8',(error,result)=>{
+    if(error){
+        console.log("errore");
+        return
+    }
+    console.log(result)
+})
