51bfd8f1fccd61333462ca89b655d939cc3d3131
Alessandro Ottone <alessandro180470@gmail.com>
2026-06-27 12:13:15 +0200

    Inizio corso node introduzione moduli

---
+++ index.js
@@ -0,0 +1,7 @@
+const saluta = require('./utils')
+const nomi = require('./nomi')
+
+//saluta("Mario")
+//saluta (persona1)
+//saluta(persona2)
+console.log(nomi)
---
+++ nomi.js
@@ -0,0 +1,3 @@
+const persona1 = "Alessandro"
+const persona2 = "Katia"
+module.exports = {persona1,persona2}
