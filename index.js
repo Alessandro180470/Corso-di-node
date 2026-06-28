b755ec9a41337c36582232ca75ef5f36e6dc8966
Alessandro Ottone <alessandro180470@gmail.com>
2026-06-28 12:02:21 +0200

    Variato index per studio del modulo http per creare un server

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-// modulo fs file system
-//const{readFileSync,writeFileSync} = require('fs')
-//const prova = readFile('./cartella/prova.txt','utf8');
-/* const ciao = readFileSync('./cartella/ciao.txt','utf8');
+/* Modulo HTTP*/
+/*const http = require('http')
+const server = http.createServer((req,res)=>{
+    if(req.url=== "/"){
 
-console.log(ciao)
-console.log(prova)
-
-writeFileSync('./cartella/prova.txt',' abito in via enrico mazzoccolo',{flag: 'a'})//inserendo uno scritto quest'ultimo va a sovrascrivere su quello vecchio
-
-
-writeFileSync('./cartella/alessandro.txt','Ciao da Alessandro');//per creare una nuova cartella 
-
- */
-const{readFile,writeFile} = require('fs')
-readFile('./cartella/prova.txt','utf8',(error,result)=>{
-    if(error){
-        console.log("errore");
-        return
+        res.end("Benvenuto sul mio sito");
     }
-    console.log(result)
+    if(req.url === "/alessandro"){
+
+        res.end("il profilo di alessandro");
+    }
+    res.end("Ciao")
 })
+server.listen(3000);*/
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+
+    if (req.url === "/") {
+        res.end("Benvenuto sul mio sito");
+        return;
+    }
+
+    if (req.url === "/alessandro") {
+        res.end("il profilo di alessandro");
+        return;
+    }
+
+    res.end("Ciao");
+})
+
+server.listen(3000);
+
