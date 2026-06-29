a089aa02232914cca9fca25961a8c34161de7239
Alessandro Ottone <alessandro180470@gmail.com>
2026-06-29 18:51:11 +0200

    Lezione su modulo json

---
+++ persone.js
@@ -0,0 +1,25 @@
+const persone = [
+    {
+        nome :"luca",
+        cognome : "rossi",
+        eta : 25,
+        indirizzo : {
+            citta: "milano",
+            indirizzo: "via torino",
+            cap: "00175"
+        },
+        interessi:["basket","calcio"]
+    },
+    {
+    nome :"marco",
+        cognome : "verdi",
+        eta : 21,
+        indirizzo : {
+            citta: "torino",
+            indirizzo: "via milano",
+            cap: "00178"
+        },
+        interessi:["pallavolo","pallamano"]}
+
+]
+module.exports={persone}
